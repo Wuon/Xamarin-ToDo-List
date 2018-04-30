fc0fb964c0a4599364e6af2833f8460877db94ba
Daniel Wu <wuon@protonmail.com>
2018-04-30 16:15:28 -0400

    Complete

---
+++ App/App/MainActivity.cs
@@ -0,0 +1,80 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Content;
+using System;
+using App.Helper;
+using System.Collections.Generic;
+
+namespace App
+{
+    [Activity(Label = "App", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Theme.AppCompat.Light")]
+    public class MainActivity : AppCompatActivity
+    {
+
+        EditText taskEditText;
+        DbHelper dbHelper;
+        CustomAdapter adapter;
+        ListView lstTask;
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_item, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_add:
+                    taskEditText = new EditText(this);
+                    Android.Support.V7.App.AlertDialog dialog = new Android.Support.V7.App.AlertDialog.Builder(this)
+                        .SetTitle("Add New Task")
+                        .SetMessage("What do you want to do next?")
+                        .SetView(taskEditText)
+                        .SetPositiveButton("Add", OkAction)
+                        .SetNegativeButton("Cancel", CancelAction)
+                        .Create();
+                    dialog.Show();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void CancelAction(object sender, DialogClickEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OkAction(object sender, DialogClickEventArgs e)
+        {
+            string task = taskEditText.Text;
+            dbHelper.InsertNewTask(task);
+            LoadTaskList();
+        }
+
+        public void LoadTaskList()
+        {
+            List<string> taskList = dbHelper.getTaskList();
+            adapter = new CustomAdapter(this, taskList, dbHelper);
+            lstTask.Adapter = adapter;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            dbHelper = new DbHelper(this);
+            lstTask = FindViewById<ListView>(Resource.Id.lstTask);
+
+            LoadTaskList();
+        }
+    }
+}
+
